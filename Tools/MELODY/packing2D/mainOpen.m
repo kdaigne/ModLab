27706ce930ea583739b91131082416c2960359d7
kdaigne <113102338+kdaigne@users.noreply.github.com>
2024-09-29 19:54:19 +0200

    Add files via upload

--- Tools/MELODY/packing2D/mainOpen.m
+++ Tools/MELODY/packing2D/mainOpen.m
@@ -7,6 +7,7 @@
 %
 %% - Abstract -
 % Runs Packing2D and transfers files to the simulation directory
+% IMPORTANT: run Packing2D from this file
 %% -
 
 % #. Paths
@@ -25,9 +26,20 @@ if ~isfile([pathGUI filesep 'Save' filesep 'path_save.mat'])
 else
     load([pathGUI filesep 'Save' filesep 'path_save.mat']); % pathSimu
 end
-% #.#. Preprocessing path
-% Outputs are saved in a preprocessing folder inside the simulation path
-pathPrepro=[pathSimu filesep 'PREPROCESSING'];
+% #.#. Paths
+pathCode=[pathSimu filesep 'CODE'];
+pathSave=[pathSimu filesep 'SAVE'];
+pathPrepro=[pathSimu filesep 'PREPROCESSING']; % Outputs are saved in a preprocessing folder inside the simulation path
+% #.#. Dir
+if exist(pathCode,'dir')==0
+    mkdir(pathCode);
+end
+if exist(pathSave,'dir')==0
+    mkdir(pathSave);
+end
+if exist(pathPrepro,'dir')==0
+    mkdir(pathPrepro);
+end
 % #.#. Validation
 answer=questdlg(['Current simulation path:' newline pathSimu newline 'Warning: duplicate files will be deleted'], ...
                     'Simulation path','Yes','No','No');
@@ -39,7 +51,24 @@ dirOld=dir([pathProgram filesep '**' filesep '*.*']);
 dirOld(ismember({dirOld.name},{'.' '..' '.DS_Store' 'Thumbs.db'}))=[];
 
 % #. Run
-run([pathProgram filesep 'Packing2D_Main_Program.m']);
+% Several processing mainly because clearvars inside Packing2D;
+% Variables are saved in a .mat which avoids having to modify the
+% Packing2D (i.e. remove clearvars) every time it's updated
+% #.#. Pre-processing
+subDirs=genpath(pathProgram); % Adds subdirectories
+addpath(subDirs);
+save([pathPrepro filesep 'Packing2DPreprocessingData.mat']);
+% #.#. Processing
+run([pathProgram filesep 'Packing2D_Main_Program']);
+% #.#. Post-processing
+pathMain=mfilename('fullpath');
+ind=strfind(lower(pathMain),'tools');
+if isempty(ind); return; end
+pathGUI=pathMain(1:ind(end)-2);
+load([pathGUI filesep 'Save' filesep 'path_save.mat']); % pathSimu
+pathPrepro=[pathSimu filesep 'PREPROCESSING'];
+load([pathPrepro filesep 'Packing2DPreprocessingData.mat']); % Retrieves variables before run
+delete([pathPrepro filesep 'Packing2DPreprocessingData.mat']);
 
 % #. Directory new
 dirNew=dir([pathProgram filesep '**' filesep '*.*']);
@@ -67,15 +96,18 @@ pathsUpdated=pathsCommon(indUpdated);
 pathsCreated=setdiff(pathsNew,pathsOld);
 
 % #. Transfer
-if ~exist(pathPrepro,'dir')
-    mkdir(pathPrepro);
-end
 for fileNum=1:numel(pathsUpdated)
     copyfile(pathsUpdated{fileNum},pathPrepro);
 end
 for fileNum=1:numel(pathsCreated)
     movefile(pathsCreated{fileNum},pathPrepro);
 end
+
+% #. Log
+[~,copyMsg]=fileparts(pathsUpdated);
+[~,moveMsg]=fileparts(pathsCreated);
+logMsg=['Files copied after computation:' reshape(copyMsg,1,[]) reshape(moveMsg,1,[])];
+[~,~]=LogSave(logMsg,{''},'Packing2D has ended normaly',[pathSimu filesep 'SAVE' filesep 'LOG_save.log']);
 
 % #. End
 msgbox('Packing2D has ended normaly.', 'Icon','help');
